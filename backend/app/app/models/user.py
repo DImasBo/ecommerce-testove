0258fd2282fd532a4000fbf081183edf5ee555c2
Dmytro Bondar <dimasbo.work@gmail.com>
2022-09-14 22:49:15 +0300

    refactor: cleared prodject (#1)
    * refactor: removed default files
    
    * refactor: removed code about items model

--- backend/app/app/models/user.py
+++ backend/app/app/models/user.py
@@ -1,13 +1,6 @@
-from typing import TYPE_CHECKING
-
 from enum import Enum
 from sqlalchemy import Boolean, Column, Integer, String
-from sqlalchemy.orm import relationship
-
 from app.db.base_class import Base
-
-if TYPE_CHECKING:
-    from .item import Item  # noqa: F401
 
 
 class RoleUser(str, Enum):
@@ -23,5 +16,4 @@ class User(Base):
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean(), default=True)
     is_superuser = Column(Boolean(), default=False)
-    items = relationship("Item", back_populates="owner")
     role = Column(String, default=RoleUser.accountant)
